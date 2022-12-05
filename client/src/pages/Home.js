dc7f30f0971c28384f6fc55776051a6a6f969cb6
Younes Mel <103428027+Youn35-Mel@users.noreply.github.com>
2022-12-05 13:46:02 +0000

    table setup

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,8 +10,8 @@ const Home = () => {
   const loadData = async () => {
     const response = await axios.get("http://localhost:5000/api/get");
     setData(response.data);
-    console.log(response);
-    // console.log(response.data);
+    // console.log(response);
+    console.log(response.data);
   };
 
   useEffect(() => {
@@ -23,9 +23,36 @@ const Home = () => {
       <table className="styled-table">
         <thead>
           <tr>
-            <th style></th>
+            <th style={{ textAlign: "center" }}>No .</th>
+            <th style={{ textAlign: "center" }}>Name</th>
+            <th style={{ textAlign: "center" }}>Email</th>
+            <th style={{ textAlign: "center" }}>Contact</th>
+            <th style={{ textAlign: "center" }}>Action</th>
           </tr>
         </thead>
+        <tbody>
+          {data.map((item, index) => {
+            return (
+              <tr key={item.id}>
+                {/* this line adds another row */}
+                <th scope="row">{index + 1}</th>
+
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.contact}</td>
+                <td>
+                  <Link to={`/update/${item.id}`}>
+                    <button className="btn btn-edit">Edit</button>
+                  </Link>
+                  <button className="btn btn-delete">Delete</button>
+                  <Link to={`/view/${item.id}`}>
+                    <button className="btn btn-view">View</button>
+                  </Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
